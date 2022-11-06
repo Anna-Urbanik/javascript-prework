b580905ccdcc7328e2522a4b8878c52192e74a71
Anna Urbanik <urbanik.ania@gmail.com>
2022-11-06 19:38:58 +0100

    Correct script.js again

--- js/script.js
+++ js/script.js
@@ -1,8 +1,3 @@
-let playerMove = 'papier';
-
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
-
-
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber);
