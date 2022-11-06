4fe4f3577af06e46536c100e322b1fff9f7f1b93
Anna Urbanik <urbanik.ania@gmail.com>
2022-11-06 22:50:04 +0100

    Add last changes

--- js/functions.js
+++ js/functions.js
@@ -7,7 +7,3 @@ function printMessage(msg){
 function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
-
-let playerMove = `papier`;
-
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
